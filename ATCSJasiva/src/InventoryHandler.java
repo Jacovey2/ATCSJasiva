507e4660c386e238416109980718847517faa558
Jacovey <jaspercovey@gmail.com>
2018-04-17 23:30:11 -0400

    up

--- ATCSJasiva/src/InventoryHandler.java
+++ ATCSJasiva/src/InventoryHandler.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
 
 public class InventoryHandler {
-	public ArrayList<Car> Cars;
 	public ArrayList<Reservation> Reservations;
+	public ArrayList<Car> Cars;
+	
+	public boolean addReservation(Car car, Location location, TimeSlot timeSlot) {
+		boolean valid=true;
+		for (Reservation r:Reservations) {
+			if(r.){
+				
+			}
+		}
+		
+		return true;
+	}
 	
 }
