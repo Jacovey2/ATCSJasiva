b817df362df0603431f0865dc7a80450b9407e25
ivfithian <ivfithian@packer.edu>
2018-04-20 17:23:29 -0400

    up

--- ATCSJasiva/src/GUI.java
+++ ATCSJasiva/src/GUI.java
@@ -71,7 +71,6 @@ public class GUI extends JFrame {
 				l.setLabelFor(textField);
 				p.add(textField);
 			}
-
 		}
 
 		// Lay out the panel.
@@ -152,10 +151,19 @@ public class GUI extends JFrame {
 
 		enter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				if (text.get(0).getText().equals("john123") && text.get(1).getText().equals("1234")) {
-					System.out.println("success");
-				} else {
+				boolean valid = false;
+				for (int i = 0; i < c.Users.size(); i++) {
+					if (text.get(0).getText().equals(c.Users.get(i).getUsername())
+							&& text.get(1).getText().equals(c.Users.get(i).getPassword())) {
+						System.out.println("success");
+						valid = true;
+					}
+				}
+				if (valid == false) {
 					System.out.println("Incorrect Username or Password");
+				}
+				for (int i = 0; i < 2; i++) {
+					text.get(i).setText("");
 				}
 			}
 		});
@@ -173,8 +181,14 @@ public class GUI extends JFrame {
 					User user = new User(text2.get(0).getText(), text2.get(1).getText(), text2.get(2).getText(),
 							text2.get(3).getText());
 					c.add(user);
-				}else {
+					for (int i = 0; i < 4; i++) {
+						text2.get(i).setText("");
+					}
+				} else {
 					System.out.println("Invalid username or password");
+					for (int i = 2; i < 4; i++) {
+						text2.get(i).setText("");
+					}
 				}
 			}
 		});
@@ -187,199 +201,14 @@ public class GUI extends JFrame {
 		this.add(enter);
 		this.add(enter2);
 
+		this.setTitle("Button in Action");
+		this.setSize(900, 375);
+		this.setVisible(true);
+
 		/*
 		 * this.add(Info); this.add(About); this.add(AboutButton); this.add(Cars);
 		 * this.add(Rent);
 		 */
-		this.setTitle("Button in Action");
-		this.setSize(900, 375);
-		this.setVisible(true);
-	}
-	/*
-	 * add.addActionListener(new ActionListener() { // add objects to store public
-	 * void actionPerformed(ActionEvent evt) { Item item = new
-	 * Item(Integer.parseInt(loc2.getText()), Integer.parseInt(siz2.getText()),
-	 * Double.parseDouble(pri2.getText()), num1.getText(),
-	 * Integer.parseInt(inv2.getText()), Integer.parseInt(id2.getText()));
-	 * store.add(item); list.setText("Current Items: " + store.findInventory()); }
-	 * });
-	 * 
-	 * remove.addActionListener(new ActionListener() { // removes object from store
-	 * public void actionPerformed(ActionEvent evt) { int i =
-	 * store.findItembyName(num1.getText()); store.remove(i);
-	 * list.setText("Current Items: " + store.findInventory()); } });
-	 * 
-	 * find.addActionListener(new ActionListener() { // finds the information of a
-	 * specific item public void actionPerformed(ActionEvent evt) {
-	 * num3.setText(store.SInfo(num1.getText())); } });
-	 * 
-	 * find2.addActionListener(new ActionListener() { // finds all in stock items
-	 * public void actionPerformed(ActionEvent evt) {
-	 * num3.setText(store.findInStock()); } });
-	 * 
-	 * info.addActionListener(new ActionListener() { // finds all info of all items
-	 * public void actionPerformed(ActionEvent evt) { num3.setText(store.Info()); }
-	 * });
-	 * 
-	 * find3.addActionListener(new ActionListener() { // finds out of stock items
-	 * public void actionPerformed(ActionEvent evt) {
-	 * num3.setText(store.findOutofStock()); } });
-	 * 
-	 * cl.addActionListener(new ActionListener() { // clears all txt fields public
-	 * void actionPerformed(ActionEvent evt) { num1.setText(""); //num2.setText("");
-	 * num3.setText(""); loc2.setText(""); siz2.setText(""); id2.setText("");
-	 * pri2.setText(""); id2.setText(""); inv2.setText(""); } });
-	 * 
-	 * write.addActionListener(new ActionListener() { // write info to file public
-	 * void actionPerformed(ActionEvent evt) { store.writeFile(); } });
-	 * 
-	 * ex.addActionListener(new ActionListener() { // quits program public void
-	 * actionPerformed(ActionEvent evt) { System.exit(0); } });
-	 */
 
-	/*
-	 * import java.awt.Color;
-	 * 
-	 * 
-	 * public class WarehouseInterface extends JFrame implements ActionListener {
-	 * private String State = "CustomerLogin"; //Constants (Change for your system)
-	 * static String DATABASENAME = "InventoryDatabase"; static String
-	 * DATABASEDIRECTORY = "/Users/jacovey/Documents/WarehouseProject/"; //Login
-	 * String public String loginStr; //text fields (must be globally accessible)
-	 * private JTextField LoginStrTF; private JTextField NameField; private
-	 * JTextField PPUField; private JTextField Location1Field; private JTextField
-	 * Location2Field; //Inventory Handler (aka "Container") public static
-	 * InventoryHandler IH;
-	 * 
-	 * public WarehouseInterface (String state) { //Defaults to customer login, but
-	 * can be put into another mode by instantiating it with a state variable if
-	 * (!state.equals("")) { State=state; } //Login Page
-	 * if(State.equals("CustomerLogin")) { //Setup this.setLayout(new
-	 * GridLayout(3,1));
-	 * 
-	 * //Login Buttons JButton lo = new JButton("Login as Customer"); JButton lom =
-	 * new JButton("Switch to Manager login"); JButton ex = new JButton("Exit");
-	 * 
-	 * //Listeners lo.addActionListener(this); lom.addActionListener(this);
-	 * ex.addActionListener(this);
-	 * 
-	 * //Adding all components to the GUI this.add(lo); this.add(lom); this.add(ex);
-	 * } else if (State.equalsIgnoreCase("ManagerLogin")) { //Setup
-	 * this.setLayout(new GridLayout(4,1));
-	 * 
-	 * //Login Buttons JButton lo = new JButton("Login as Manager"); JButton loc =
-	 * new JButton("Switch to Customer login"); JButton ex = new JButton("Exit");
-	 * 
-	 * //Listeners lo.addActionListener(this); loc.addActionListener(this);
-	 * ex.addActionListener(this);
-	 * 
-	 * //Panels JPanel pan1= new JPanel();
-	 * 
-	 * //Login String Text Field LoginStrTF = new JPasswordField(20);
-	 * LoginStrTF.setSize(100,100); LoginStrTF.setEditable(true);
-	 * 
-	 * //Adding all components to the GUI this.add(pan1); pan1.add(LoginStrTF);
-	 * this.add(lo); this.add(loc); this.add(ex); }
-	 * 
-	 * else if (State.equals("Manager")) { //Manger logins this.setLayout(new
-	 * GridLayout(1,2)); JPanel NavigationPanel = new JPanel(); JPanel
-	 * InterfacesPanel = new JPanel(); JPanel AddingPanel = new JPanel(); JPanel
-	 * MovingPanel = new JPanel(); JPanel ItemsPanel = new JPanel();
-	 * 
-	 * //TopButtons JButton backToLogin = new JButton("Exit to menu"); JButton
-	 * quitOut = new JButton("Exit"); backToLogin.addActionListener(this);
-	 * quitOut.addActionListener(this); NavigationPanel.add(backToLogin);
-	 * NavigationPanel.add(quitOut);
-	 * 
-	 * //AddingFields JLabel NameFieldLabel = new JLabel("Name:"); NameField = new
-	 * JTextField(); JLabel PPUFieldLabel = new JLabel("PPU:"); PPUField = new
-	 * JTextField(); JButton Add = new JButton("Add");
-	 * 
-	 * //MovingFields JLabel Loc1FieldLabel = new JLabel("Location 1:");
-	 * Location1Field = new JTextField(); JLabel Loc2FieldLabel = new
-	 * JLabel("Location 2:"); Location2Field = new JTextField(); JButton Move = new
-	 * JButton("Move");
-	 * 
-	 * //Adding AddingPanel.setLayout(new GridLayout(7,1));
-	 * AddingPanel.add(NameFieldLabel); AddingPanel.add(NameField);
-	 * AddingPanel.add(PPUFieldLabel); AddingPanel.add(PPUField);
-	 * Add.addActionListener(this); AddingPanel.add(Add); //Moving
-	 * MovingPanel.setLayout(new GridLayout(5,1)); MovingPanel.add(Loc1FieldLabel);
-	 * MovingPanel.add(Location1Field); MovingPanel.add(Loc2FieldLabel);
-	 * MovingPanel.add(Location2Field); Move.addActionListener(this);
-	 * MovingPanel.add(Move);
-	 * 
-	 * //Merging InterfacesPanel.setLayout(new GridLayout(3,1));
-	 * InterfacesPanel.add(NavigationPanel); InterfacesPanel.add(AddingPanel);
-	 * InterfacesPanel.add(MovingPanel);
-	 * 
-	 * //Items ItemsPanel.setLayout(new BoxLayout(ItemsPanel,1)); for (int i=0;
-	 * i<IH.count(); i++) { JLabel tempLabel = new
-	 * JLabel((i+1)+" - "+IH.getItemAt(i).toString());
-	 * tempLabel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 * ItemsPanel.add(new JLabel((i+1)+" - "+IH.getItemAt(i).toString())); }
-	 * //Borders
-	 * InterfacesPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 * ItemsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 * AddingPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 * MovingPanel.setBorder(BorderFactory.createLineBorder(Color.black)); //Final
-	 * Pasting this.add(InterfacesPanel); this.add(ItemsPanel); } else if
-	 * (State.equals("Customer")) { this.setLayout(new GridLayout(3,1)); //Top Panel
-	 * JPanel topPanel = new JPanel(); topPanel.setLayout(new GridLayout(2,1));
-	 * topPanel.add(new JLabel("Ikea Warehouse", SwingConstants.CENTER)); JPanel
-	 * tableHeader= new JPanel(); tableHeader.setLayout(new GridLayout(1,5));
-	 * tableHeader.add(new JLabel("Name")); tableHeader.add(new JLabel("Price"));
-	 * tableHeader.add(new JLabel("ID")); tableHeader.add(new JLabel("Location"));
-	 * tableHeader.add(new JLabel("Buy Option")); topPanel.add(tableHeader);
-	 * this.add(topPanel); JPanel bottomPanels = new JPanel();
-	 * bottomPanels.setLayout(new GridLayout(1,2)); JPanel bottomPanel = new
-	 * JPanel(); bottomPanel.setLayout(new BoxLayout(bottomPanel,1)); //List of
-	 * items (long grid layout) for (int i=0; i<IH.count(); i++) { JPanel tempPanel
-	 * = new JPanel(); tempPanel.setLayout(new GridLayout(1,5)); tempPanel.add(new
-	 * JLabel(IH.getItemAt(i).iD+"")); tempPanel.add(new
-	 * JLabel(IH.getItemAt(i).name)); tempPanel.add(new
-	 * JLabel(IH.getItemAt(i).location+"")); tempPanel.add(new
-	 * JLabel(IH.getItemAt(i).PPU+"")); JButton tempButton = new JButton
-	 * ("Buy "+IH.getItemAt(i).name); tempButton.addActionListener(this);
-	 * tempPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-	 * tempPanel.add(tempButton); bottomPanel.add(tempPanel); }
-	 * bottomPanels.add(bottomPanel); this.add(bottomPanels);
-	 * 
-	 * //Exit button JButton menuExit = new JButton("Exit to menu");
-	 * menuExit.addActionListener(this); this.add(menuExit); } //Window Setup Final
-	 * this.setTitle("Warehouse Interface"); this.setSize(500, 500);
-	 * this.setVisible(true); }
-	 * 
-	 * public static void main (String[] args) { new WarehouseInterface(""); IH =
-	 * new InventoryHandler();//stays constant }
-	 * 
-	 * public void actionPerformed(ActionEvent e) { String str =
-	 * e.getActionCommand();
-	 * 
-	 * if(str.equals("Login as Customer")) {//Customer Login this.dispose();
-	 * IH.loadInventory(DATABASENAME, DATABASEDIRECTORY); new
-	 * WarehouseInterface("Customer"); } else if
-	 * (str.equals("Switch to Manager login")) {//Switch to Manager Login
-	 * this.dispose(); new WarehouseInterface("ManagerLogin"); } else if
-	 * (str.equals("Login as Manager")) { if
-	 * (LoginStrTF.getText().equals("password123")) {//Very secure password
-	 * this.dispose(); IH.loadInventory(DATABASENAME, DATABASEDIRECTORY); new
-	 * WarehouseInterface("Manager"); } } else if
-	 * (str.equals("Switch to Customer login")) {//Switch to Customer Login
-	 * this.dispose(); new WarehouseInterface("CustomerLogin"); } else if
-	 * (str.equals("Add")) { //Manager Add Button
-	 * IH.addItem(NameField.getText(),Double.parseDouble(PPUField.getText()));
-	 * this.dispose(); new WarehouseInterface("Manager"); } else
-	 * if(str.equals("Move")) { //Manager Move Button
-	 * IH.forklift(Integer.parseInt(Location1Field.getText()),
-	 * Integer.parseInt(Location2Field.getText())); this.dispose(); new
-	 * WarehouseInterface("Manager"); } else if(str.equals("Exit to menu")) { //Exit
-	 * to Menu Button IH.saveInventory(DATABASENAME, DATABASEDIRECTORY);
-	 * this.dispose(); new WarehouseInterface("CustomerLogin"); } else
-	 * if(str.startsWith("Buy")) {//Buying Button(s?)
-	 * IH.removeItem(str.substring(4)); this.dispose(); new
-	 * WarehouseInterface("Customer"); } else if(str.equals("Exit")) {//Exit App
-	 * Button IH.saveInventory(DATABASENAME, DATABASEDIRECTORY); System.exit(0); } }
-	 * }
-	 */
+	}
 }
