98e56af918634acc7e01375558765089e0c9eab1
Jacovey <jaspercovey@gmail.com>
2018-04-25 09:41:37 -0400

    update

--- ATCSJasiva/src/Reservation.java
+++ ATCSJasiva/src/Reservation.java
@@ -1,4 +1,21 @@
 
 public class Reservation {
-
+	private TimeSlot timeSlot;
+	private Car car;
+	private User user;
+	
+	public Reservation(TimeSlot timeSlotx, Car carx, User userx) {
+		timeSlot = timeSlotx;
+		car=carx;
+		user=userx;
+	}
+	public TimeSlot getTimeSlot() {
+		return timeSlot;
+	}
+	public Car getCar() {
+		return car;
+	}
+	public User getUser() {
+		return user;
+	}
 }
