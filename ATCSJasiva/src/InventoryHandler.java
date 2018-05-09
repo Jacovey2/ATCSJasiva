0b6b40c68872d1353c82b4da86a707b13fc14c7a
Jacovey <jaspercovey@gmail.com>
2018-05-09 10:21:49 -0400

    up

--- ATCSJasiva/src/InventoryHandler.java
+++ ATCSJasiva/src/InventoryHandler.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class InventoryHandler {
 	public ArrayList<Reservation> Reservations = new ArrayList<Reservation>();
 	public ArrayList<Car> Cars = new ArrayList<Car>();
@@ -23,7 +22,7 @@ public class InventoryHandler {
 		reservationFile = new File("reservations.txt");
 		carFile = new File("cars.txt");
 		usersFile = new File("users.txt");
-		
+
 		// Write Reservations to file
 		FileWriter rFileWrite = new FileWriter(reservationFile);
 		for (Reservation r : Reservations) {
@@ -34,7 +33,7 @@ public class InventoryHandler {
 		// Write Cars
 		FileWriter cFileWrite = new FileWriter(carFile);
 		for (Car c : Cars) {
-			cFileWrite.write(c.toString()+"\n");
+			cFileWrite.write(c.toString() + "\n");
 		}
 		cFileWrite.close();
 
@@ -52,7 +51,7 @@ public class InventoryHandler {
 		Scanner rScanner = new Scanner(reservationFile);
 		rScanner.useDelimiter("/|\\n");
 		while (rScanner.hasNextLine()) {
-			//creating timeslot from line
+			// creating timeslot from line
 			int startMonth = rScanner.nextInt();
 			int startDay = rScanner.nextInt();
 			int startYear = rScanner.nextInt();
@@ -62,24 +61,26 @@ public class InventoryHandler {
 			int endYear = rScanner.nextInt();
 			int endTime = rScanner.nextInt();
 			TimeSlot ts = new TimeSlot(startMonth, startDay, startYear, startTime, endMonth, endDay, endYear, endTime);
-			
-			//Creating car from line
-			String model =        rScanner.next();
-			Location location =   new Location(rScanner.next());
-			int mpg =             rScanner.nextInt();
-			int capacity =        rScanner.nextInt();
+
+			// Creating car from line
+			String model = rScanner.next();
+			Location location = new Location(rScanner.next());
+			int mpg = rScanner.nextInt();
+			int capacity = rScanner.nextInt();
 			double pricePerHour = rScanner.nextDouble();
 			Car car = new Car(mpg, capacity, model, location, pricePerHour);
-			
-			//creating user from line
+
+			// creating user from line
 			String firstName = rScanner.next();
 			String lastName = rScanner.next();
-			String username = rScanner.next();;
-			String password = rScanner.next();;
-			User user = new User(firstName,lastName,username,password);
-			
-			//Creating Reservation from read data
-			Reservations.add(new Reservation(ts,car,user));
+			String username = rScanner.next();
+			;
+			String password = rScanner.next();
+			;
+			User user = new User(firstName, lastName, username, password);
+
+			// Creating Reservation from read data
+			Reservations.add(new Reservation(ts, car, user));
 		}
 		rScanner.close();
 
@@ -88,13 +89,13 @@ public class InventoryHandler {
 		Scanner cScanner = new Scanner(carFile);
 		cScanner.useDelimiter("/|\\n"); // notation for "use both / and \n as delimiters"
 		while (cScanner.hasNext()) {
-			//Creating car from line
-			String model =        cScanner.next();
-			Location location =   new Location(cScanner.next());
-			int mpg =             cScanner.nextInt();
-			int capacity =        cScanner.nextInt();
+			// Creating car from line
+			String model = cScanner.next();
+			Location location = new Location(cScanner.next());
+			int mpg = cScanner.nextInt();
+			int capacity = cScanner.nextInt();
 			double pricePerHour = cScanner.nextDouble();
-			//adding car to list 
+			// adding car to list
 			Cars.add(new Car(mpg, capacity, model, location, pricePerHour));
 		}
 		cScanner.close();
@@ -104,31 +105,33 @@ public class InventoryHandler {
 		Scanner uScanner = new Scanner(usersFile);
 		uScanner.useDelimiter("/|\\n");
 		while (uScanner.hasNextLine()) {
-			//creating user from line
+			// creating user from line
 			String firstName = uScanner.next();
 			String lastName = uScanner.next();
 			String username = uScanner.next();
 			String password = uScanner.next();
-			//adding user to list
-			Users.add(new User(firstName,lastName,username,password));
+			// adding user to list
+			Users.add(new User(firstName, lastName, username, password));
 		}
 		uScanner.close();
 	}
 
 	public double addReservation(Car car, Location loc1, Location loc2, TimeSlot timeSlot, User user) {
-		boolean valid = true;
-		for (Reservation r : Reservations) { //this gives the booking issues
-			if (r.getTimeSlot().Conflict(timeSlot) && r.getCar().location.locationName.equals(loc1.locationName) && r.getCar().model.equals(car.model))
-				valid = false;
-		}
-		boolean ValidCarAtLocation =false;
-		for (Car c:Cars) {
-			System.out.println(c.location.toString().equals(loc1.toString()) + "  "+c.model.equals(car.model));
+		ArrayList<Car> validCarsAtLocation = new ArrayList<Car>();
+		for (Car c : Cars) {//Seems to work -NEED TO TEST
 			if (c.location.toString().equals(loc1.toString()) && c.model.equals(car.model)) {
-				ValidCarAtLocation=true;
+				boolean addCar = true; 
+				for (Reservation r : Reservations) {
+					System.out.println(r.getCar().Equals(c) +": ("+c.toNiceString() +") == ("+r.getCar().toNiceString()+")");
+					if ( r.getCar().Equals(c) && r.getTimeSlot().Conflict(timeSlot)) 
+						addCar=false;
+				}
+				if (addCar)
+					validCarsAtLocation.add(c);
 			}
 		}
-		if (valid && ValidCarAtLocation) {
+
+		if (validCarsAtLocation.size()>0) {
 			Reservations.add(new Reservation(timeSlot, car, user));
 			double durationDiscountRate = 0.15 / 30;
 			double maxDiscount = 0.5;
@@ -136,10 +139,12 @@ public class InventoryHandler {
 			double x = durationInHours;
 			double r = durationDiscountRate;
 			double mD = maxDiscount;
-			double gx = x*r;
-			double fx = ((2*mD)/(1+Math.pow(Math.E, -4*gx))) - mD;//sigmoid curve (sigmoid curve has default derivative of 1/4 at 0, thus multiplying by 4 to counteract)
-			double dx = 1-fx;
-			double price = car.pricePerHour*x*dx;// TODO: add multiple location pricing
+			double gx = x * r;
+			double fx = ((2 * mD) / (1 + Math.pow(Math.E, -4 * gx))) - mD;// sigmoid curve (sigmoid curve has default
+																			// derivative of 1/4 at 0, thus multiplying
+																			// by 4 to counteract)
+			double dx = 1 - fx;
+			double price = car.pricePerHour * x * dx;// TODO: add multiple location pricing
 			price += loc1.distanceFrom(loc2) * 0.5;
 			return price;
 		} else {
@@ -166,6 +171,7 @@ public class InventoryHandler {
 		}
 		return tempArray;
 	}
+
 	public ArrayList<String> getLocations() {
 		ArrayList<String> tempArray = new ArrayList<String>();
 		for (Car c : Cars) {
@@ -173,6 +179,7 @@ public class InventoryHandler {
 		}
 		return Collapse(tempArray);
 	}
+
 	public ArrayList<String> Collapse(ArrayList<String> arrayList) {
 		ArrayList<String> tempString = new ArrayList<String>();
 		for (String S : arrayList)
@@ -180,49 +187,51 @@ public class InventoryHandler {
 				tempString.add(S);
 		return tempString;
 	}
+
 	public String[] getLocationsArray() {
 		String[] list = new String[Cars.size()];
-		for(int i = 0; i<Cars.size();i++) {
-			list[i]=Cars.get(i).location.toString();
-			System.out.print(list[i] + " ");
+		for (int i = 0; i < Cars.size(); i++) {
+			list[i] = Cars.get(i).location.toString();
 		}
-		System.out.println();
 		return list;
 	}
-	
+
 	public String getReservationsString() {
-		String list="";
-		int counter =1;
+		String list = "";
+		int counter = 1;
 		for (Reservation c : Reservations) {
-			list += "("+counter+") "+c.toNiceString()+"\n";
+			list += "(" + counter + ") " + c.toNiceString() + "\n";
 			counter++;
 		}
 		return list;
 	}
+
 	public String getCarsString() {
-		String list="";
-		int counter =1;
+		String list = "";
+		int counter = 1;
 		for (Car c : Cars) {
-			list+="("+counter+") "+c.toNiceString()+"\n";
+			list += "(" + counter + ") " + c.toNiceString() + "\n";
 			counter++;
 
 		}
 		return list;
 	}
+
 	public String getUsersString() {
-		String list="";
-		int counter =1;
+		String list = "";
+		int counter = 1;
 		for (User u : Users) {
-			list+="("+counter+") "+u.toNiceString()+"\n";
+			list += "(" + counter + ") " + u.toNiceString() + "\n";
 			counter++;
 		}
 		return list;
 	}
+
 	public String getLocationsString() {
-		String list="";
-		int counter =1;
+		String list = "";
+		int counter = 1;
 		for (String l : this.getLocations()) {
-			list+="("+counter+") "+l+"\n";
+			list += "(" + counter + ") " + l + "\n";
 			counter++;
 		}
 		return list;
